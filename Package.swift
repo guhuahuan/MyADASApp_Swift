a4c60dbb414951a4e9f18e6c2d577014b453ef40
guhuahuan <38170271+guhuahuan@users.noreply.github.com>
2026-04-19 08:06:32 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,16 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "MyADASApp_Swift",
-    platforms: [
-        .iOS(.v17)
-    ],
+    platforms: [.iOS(.v17)],
     products: [
         .executable(name: "MyADASApp_Swift", targets: ["MyADASApp_Swift"])
     ],
     targets: [
         .executableTarget(
             name: "MyADASApp_Swift",
-            path: "Sources" // 确保你的 ContentView.swift 放在仓库的 Sources 文件夹内
+            path: "Sources",
+            resources: [
+                // 仅保留 yolov8l，请确保 Sources 文件夹下确实有这个文件夹
+                .process("yolov8l.mlpackage")
+            ]
         )
     ]
 )
