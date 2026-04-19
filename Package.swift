777156e75272ae7696d510a599cef4dabf0e9cae
guhuahuan <38170271+guhuahuan@users.noreply.github.com>
2026-04-19 07:58:20 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "MyADASApp_Swift",
-    platforms: [.iOS(.v17)],
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         .executable(name: "MyADASApp_Swift", targets: ["MyADASApp_Swift"])
     ],
     targets: [
         .executableTarget(
             name: "MyADASApp_Swift",
-            path: "Sources" // 确保你的 ContentView.swift 在 Sources 文件夹里
+            path: "Sources" // 确保你的 ContentView.swift 放在仓库的 Sources 文件夹内
         )
     ]
 )
